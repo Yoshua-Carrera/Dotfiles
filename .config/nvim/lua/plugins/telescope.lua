31892e0fb73cfedef95fc964d2f4f018cad1f9f1
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2025-06-04 20:10:55 -0400

    additional changes to improve usability

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,10 @@
 return {
   "nvim-telescope/telescope.nvim",
-  commit = "af78ba3b7da51ea5780bdb2c4b990ae2ea2ce716",
   opts = {
     defaults = {
+      cwd = function()
+        return vim.fn.getcwd()
+      end,
       path_display = { "truncate" },
       previewer = true,
       layout_strategy = "vertical",
@@ -12,17 +14,52 @@ return {
       },
       sorting_strategy = "ascending",
       winblend = 0,
-      file_ignore_patterns = { "node_modules", ".angular", ".git", "dist" },
+      file_ignore_patterns = { ".angular", ".git", "dist" },
     },
   },
-  config = function(_, opts)
-    require("telescope").setup(opts)
-
-    -- Set your keymap here
-    vim.keymap.set("n", "<leader><leader>", function()
-      require("telescope.builtin").find_files({
-        cwd = vim.fn.getcwd(),
-      })
-    end, { desc = "Telescope: (Root Dir)" })
+  config = function()
+    local builtin = require("telescope.builtin")
+    local pickers_to_patch = {
+      "find_files",
+      "live_grep",
+      "grep_string",
+      "buffers",
+      "oldfiles",
+      "quickfix",
+      "loclist",
+      "diagnostics",
+      "diagnostics",
+      "git_files",
+      "lsp_definitions",
+      "lsp_references",
+      "lsp_implementations",
+      "lsp_type_definitions",
+      "lsp_declarations",
+      "lsp_incoming_calls",
+      "lsp_outgoing_calls",
+      "lsp_code_actions",
+      "lsp_range_code_actions",
+      "lsp_document_symbols",
+      "lsp_workspace_symbols",
+      "lsp_dynamic_workspace_symbols",
+    }
+    for _, picker in ipairs(pickers_to_patch) do
+      local original = builtin[picker]
+      builtin[picker] = function(opts)
+        opts = opts or {}
+        opts.cwd = vim.fn.getcwd()
+        return original(opts)
+      end
+    end
   end,
+  -- config = function(_, opts)
+  --   require("telescope").setup(opts)
+  --
+  --   -- Set your keymap here
+  --   vim.keymap.set("n", "<leader><leader>", function()
+  --     require("telescope.builtin").find_files({
+  --       cwd = vim.fn.getcwd(),
+  --     })
+  --   end, { desc = "Telescope: (Root Dir)" })
+  -- end,
 }
