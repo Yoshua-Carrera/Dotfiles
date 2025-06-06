6de0de021c880f554f74e1c5edaf6bb763db7f8c
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2025-06-06 16:52:52 -0400

    general improvements

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,12 +1,13 @@
+---@diagnostic disable: redefined-local
 return {
   "nvim-telescope/telescope.nvim",
   opts = {
+    priority = 1000,
+    additional_args = function()
+      return { "--follow" }
+    end,
     defaults = {
-      cwd = function()
-        return vim.fn.getcwd()
-      end,
       path_display = { "truncate" },
-      previewer = true,
       layout_strategy = "vertical",
       layout_config = {
         prompt_position = "bottom",
@@ -16,46 +17,52 @@ return {
       winblend = 0,
     },
   },
-  config = function()
+  config = function(_, opts)
+    require("telescope").setup(opts)
+
     local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader><leader>", function()
-      builtin.find_files({
-        cwd = vim.fn.cwd(),
-      })
-    end, { desc = "Telescope (Root Dir)" })
-    vim.keymap.set("n", "<leader><leader>sg", function()
-      builtin.live_grep({
-        cwd = vim.fn.cwd(),
-      })
-    end, { desc = "live Grep (Root Dir)" })
-    local pickers_to_patch = {
-      "grep_string",
-      "buffers",
-      "oldfiles",
-      "quickfix",
-      "loclist",
-      "diagnostics",
-      "diagnostics",
-      "git_files",
-      "lsp_definitions",
-      "lsp_references",
-      "lsp_implementations",
-      "lsp_type_definitions",
-      "lsp_declarations",
-      "lsp_incoming_calls",
-      "lsp_outgoing_calls",
-      "lsp_code_actions",
-      "lsp_range_code_actions",
-      "lsp_document_symbols",
-      "lsp_workspace_symbols",
-      "lsp_dynamic_workspace_symbols",
-    }
-    for _, picker in ipairs(pickers_to_patch) do
-      local original = builtin[picker]
-      builtin[picker] = function(opts)
-        opts = opts or {}
-        opts.cwd = vim.fn.getcwd()
-        return original(opts)
+    if not string.match(vim.bo.filetype, "snacks_dashboard") then
+      vim.keymap.set("n", "<leader><leader>", function()
+        builtin.find_files({
+          cwd = vim.fn.getcwd(),
+          find_command = { "fd", "--type", "f", "--follow" },
+        })
+      end, { desc = "Telescope (Root Dir)" })
+      vim.keymap.set("n", "<leader>sg", function()
+        builtin.live_grep({
+          cwd = vim.fn.getcwd(),
+        })
+      end, { desc = "live Grep (Root Dir)" })
+      local pickers_to_patch = {
+        "grep_string",
+        "buffers",
+        "oldfiles",
+        "quickfix",
+        "loclist",
+        "diagnostics",
+        "lsp_definitions",
+        "lsp_references",
+        "lsp_implementations",
+        "lsp_type_definitions",
+        "lsp_declarations",
+        "lsp_incoming_calls",
+        "lsp_outgoing_calls",
+        "lsp_code_actions",
+        "lsp_range_code_actions",
+        "lsp_document_symbols",
+        "lsp_workspace_symbols",
+        "lsp_dynamic_workspace_symbols",
+      }
+
+      for _, picker in ipairs(pickers_to_patch) do
+        local original = builtin[picker]
+        if original then
+          builtin[picker] = function(opts)
+            opts = opts or {}
+            opts.cwd = vim.fn.getcwd()
+            return original(opts)
+          end
+        end
       end
     end
   end,
