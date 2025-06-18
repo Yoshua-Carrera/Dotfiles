51e5f9a3dee205b6fe59a5d566475304a4737014
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2025-06-17 21:06:19 -0400

    additional nvim configs

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -8,20 +8,29 @@ return {
     priority = 1000,
     transparent_background = true,
     dim_inactive = {
-      enabled = false, -- dims the background color of inactive window
+      enabled = false,
       shade = "dark",
-      percentage = 0.25, -- percentage of the shade to apply to the inactive window
+      percentage = 0.25,
     },
   },
   config = function(_, opts)
     require("catppuccin").setup(opts)
+
+    -- Set colorscheme first
     vim.cmd.colorscheme("catppuccin-macchiato")
-    vim.cmd([[
-      highlight Normal guibg=NONE ctermbg=NONE
-      highlight NormalNC guibg=NONE ctermbg=NONE
-      highlight Pmenu guibg=NONE ctermbg=NONE
-      highlight PmenuSel guibg=NONE ctermbg=NONE
-      highlight VertSplit guibg=NONE ctermbg=NONE
-    ]])
+
+    -- Hook into ColorScheme event to override highlight groups
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      pattern = "*",
+      callback = function()
+        local hl = vim.api.nvim_set_hl
+        hl(0, "Normal", { bg = "NONE" })
+        hl(0, "NormalNC", { bg = "NONE" })
+        hl(0, "CursorLine", { bg = "NONE" })
+        hl(0, "Pmenu", { bg = "NONE" })
+        hl(0, "PmenuSel", { bg = "NONE" })
+        hl(0, "VertSplit", { bg = "NONE" })
+      end,
+    })
   end,
 }
