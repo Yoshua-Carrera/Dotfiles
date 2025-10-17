7ac945605ddcc2221b651a60f60143c6044d9fd4
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2025-10-17 14:45:38 -0500

    update fuzzy-co script

--- .config/zsh/scripts/fuzzy-co.sh
+++ .config/zsh/scripts/fuzzy-co.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 target=$(git branch -r | sed "s|origin/||" | sed "s/^[[:space:]]*//" | fzf)
-read -r -p "run git checkout $target? (Y/n): " response
-case "$response" in
-[yY] | [yY][eE][sS] | "")
-  echo "Checking into $target"
-  git co $target
-  ;;
-[nN] | [nN][oO])
+if [ -z $target ]; then
   echo "Aborting"
-  ;;
-*)
-  echo "Invalid input. Operation aborted."
-  ;;
-esac
+
+else
+  read -r -p "run git checkout $target? (Y/n): " response
+  case "$response" in
+  [yY] | [yY][eE][sS] | "")
+    echo "Checking into $target"
+    git co $target
+    ;;
+  [nN] | [nN][oO])
+    echo "Aborting"
+    ;;
+  *)
+    echo "Invalid input. Operation aborted."
+    ;;
+  esac
+fi
